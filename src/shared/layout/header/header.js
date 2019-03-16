c73dcfd5aeacaacf7bceb85d17c54ee07157c8cd
bbdnet1168 <bbdnet1168@QINISELAM>
2019-03-16 17:06:57 +0200

    this commit will add the mobile header with side bard

--- src/shared/layout/header/header.js
+++ src/shared/layout/header/header.js
@@ -1,65 +1,58 @@
-import React, { Component } from 'react';
-import { Input, Menu, Icon, Sidebar } from 'semantic-ui-react'
-import './header.css'; // Import regular styleshe
-import { Route, Link, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import { Input, Menu, Icon, Sidebar } from "semantic-ui-react";
+import "./header.css"; // Import regular styleshe
+import { Route, Link, Switch } from "react-router-dom";
 
 class Header extends Component {
-  state = { activeItem: '', visible: false };
+  constructor(props) {
+    super(props);
+  }
 
-  handleItemClick = (e, { name }) => {
-    //this.setState({ activeItem: name }); 
-    this.setState({ visible: true })
-  };
-
-  handleHideClick = () => {
-    if (this.state.visible) {
-      this.setState({ visible: false });
-    }
-  };
-
-  handleShowClick = () => this.setState({ visible: true });
-  handleSidebarHide = () => this.setState({ visible: false });
+  handleShowClick = () => this.props.sideBarChange(true);
 
   render() {
-    const { activeItem } = this.state.activeItem;
-    const { visible } = this.state.visible;
     return (
       <div>
-        <Menu className='header-menu'>
-          <Menu.Item name='expandable' active={activeItem === 'expandable'} onClick={this.handleItemClick} className='xs-visible'>
-            <Icon name='gamepad' />
+        <Menu widths={5}>
+          <Menu.Item
+            name="expandable"
+            onClick={this.handleShowClick}
+            className="xs-visible"
+          >
+            <Icon name="align justify" />
           </Menu.Item>
-
-          <Menu.Menu position='left'>
-            <Menu.Item name='item1' active={activeItem === 'item1'} onClick={this.handleItemClick} className='xs-hidden menu-item' />
-            <Menu.Item name='item2' active={activeItem === 'item2'} onClick={this.handleItemClick} className='xs-hidden menu-item' />
-            <Menu.Item name='item3' active={activeItem === 'item3'} onClick={this.handleItemClick} className='xs-hidden menu-item' />
-            <Menu.Item name='item4' active={activeItem === 'item4'} onClick={this.handleItemClick} className='menu-item' />
-            <Menu.Item name='item5' active={activeItem === 'item5'} onClick={this.handleItemClick} className='menu-item' />
-          </Menu.Menu>
-
-          <Menu.Menu position='right'>
-            <Menu.Item><Input icon='search' placeholder='Search...' /></Menu.Item>
-            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick}
-            />
-          </Menu.Menu>
+          <Menu.Item
+            name="expandable"
+            onClick={this.handleItemClick}
+            className="xs-visible"
+          >
+            <Icon name="user" />
+          </Menu.Item>
+          <Menu.Item
+            name="expandable"
+            onClick={this.handleItemClick}
+            className="xs-visible"
+          >
+            <Icon name="wifi" />
+          </Menu.Item>
+          <Menu.Item
+            name="expandable"
+            onClick={this.handleItemClick}
+            className="xs-visible"
+          >
+            <Icon name="mail" />
+          </Menu.Item>
+          <Menu.Item
+            name="expandable"
+            onClick={this.handleItemClick}
+            className="xs-visible"
+          >
+            <Icon name="bell" />
+          </Menu.Item>
         </Menu>
-
-        <Sidebar as={Menu} animation='overlay' icon='labeled' inverted vertical visible={visible} width='thin'>
-          <Menu.Item>
-            <Link to="/login"><Icon name='home' />Home</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/register"><Icon name='gamepad' />Games</Link>
-          </Menu.Item>
-          <Menu.Item as='a'>
-            <Icon name='camera' />
-            Channels
-             </Menu.Item>
-        </Sidebar>
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default Header;
--- src/shared/layout/main/main.js
+++ src/shared/layout/main/main.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
-import Home from './components/home/home';
-import Register from './components/register/register';
-import Login from './components/login/login';
+import Home from '../../../components/home/home';
+import Register from '../../../components/register/register';
+import Login from '../../../components/login/login';
+import { Container, Icon, Menu, Sidebar, Grid } from 'semantic-ui-react';
 
 class Main extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+  handleSidebarHide = () => {
+    this.props.sideBarChange(false);
+  };
+
   render() {
-    return
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/home" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>;
+    const visible  = this.props.visible
+    return(
+    <Sidebar.Pushable className="page">
+     <Sidebar as={Menu} animation='overlay' icon='labeled' inverted onHide={this.handleSidebarHide}
+              vertical visible={visible} width='thin'>
+          <Menu.Item as='a'>Add Device</Menu.Item>
+          <Menu.Item as='a'>Buy Data</Menu.Item>
+          <Menu.Item as='a'>Sign Out</Menu.Item>
+     </Sidebar>
+
+     <Sidebar.Pusher className="page-container" onClick={this.handleHideClick}>
+        <Grid celled className="main-grid">
+          <Grid.Row className="main">
+            <Grid.Column>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/home" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+              </Switch>
+           </Grid.Column>
+         </Grid.Row>
+       </Grid>
+    </Sidebar.Pusher>
+  </Sidebar.Pushable>
+    )
   }
 }
 
